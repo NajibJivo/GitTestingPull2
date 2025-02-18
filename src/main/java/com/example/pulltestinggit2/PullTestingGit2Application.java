8402affaf01d191876b92ce8d0e858f34aaad168
NajibJivo <nana0001@stud.kea.dk>
2025-02-18 21:00:55 +0100

    second commit

---
+++ src/main/java/com/example/pulltestinggit2/PullTestingGit2Application.java
@@ -0,0 +1,13 @@
+package com.example.pulltestinggit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullTestingGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullTestingGit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pulltestinggit2/PullTestingGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pulltestinggit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullTestingGit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
